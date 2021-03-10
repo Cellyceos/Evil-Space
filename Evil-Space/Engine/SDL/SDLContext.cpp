903fc1587413a27ef5172765fafde6cbaeeab0f2
Cellyceos <brakir@gmail.com>
2021-03-11 00:09:40 +0300

    Add SDL_image initialization

--- Evil-Space/Engine/SDL/SDLContext.cpp
+++ Evil-Space/Engine/SDL/SDLContext.cpp
@@ -10,6 +10,8 @@
 
 #include "SDL.h"
 #include "SDL_ttf.h"
+#include "SDL_image.h"
+
 
 SDLContext SDLContext::Context;
 
@@ -22,17 +24,37 @@ SDLContext::SDLContext()
 		exit(1);
 	}
 
+#ifdef USE_SDL_TTF
 	if (TTF_Init() != 0)
 	{
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Unable to init SDL_ttf lib. See the log for more info.", NULL);
 		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "Unable to init SDL_ttf, error: %s", SDL_GetError());
 		exit(1);
 	}
+#endif // USE_SDL_TTF
+
+#ifdef USE_SDL_IMG
+	if (IMG_Init(IMG_INIT_TIF | IMG_INIT_PNG) == 0)
+	{
+		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Unable to init SDL_image lib. See the log for more info.", NULL);
+		SDL_LogCritical(SDL_LOG_CATEGORY_APPLICATION, "Unable to init SDL_image, error: %s", SDL_GetError());
+		exit(1);
+	}
+#endif // USE_SDL_ING
+
 }
 
 SDLContext::~SDLContext()
 {
 	SDL_Log("~SDLContext\n");
+
+#ifdef USE_SDL_IMG
+	IMG_Quit();
+#endif // USE_SDL_ING
+
+#ifdef USE_SDL_TTF
 	TTF_Quit();
+#endif // USE_SDL_TTF
+
 	SDL_Quit();
 }
